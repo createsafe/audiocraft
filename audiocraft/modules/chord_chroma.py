27cc995566944ffd9368c8cb46d94ec56823b6c8
javanasse <julian.vanasse@gmail.com>
2024-05-21 20:26:29 +0000

    fixed paths to config and weights

--- audiocraft/modules/chord_chroma.py
+++ audiocraft/modules/chord_chroma.py
@@ -17,16 +17,19 @@ import librosa
 import argparse
 import warnings
 
+BTC_CONFIG_PATH = os.path.join(os.getcwd(), "audiocraft", "modules", "btc", "run_config.yaml")
+BTC_WEIGHTS_PATH = os.path.join(os.getcwd(), "audiocraft", "modules", "btc", "test", "btc_model_large_voca.pt")
+
 class ChordExtractor(nn.Module):
 
     def __init__(self, device, sample_rate, max_duration, chroma_len, n_chroma, winhop):
         super().__init__()
-        self.config = HParams.load("/src/audiocraft/modules/btc/run_config.yaml") #gotta specify the path for run_config.yaml of btc
+        self.config = HParams.load(BTC_CONFIG_PATH) #gotta specify the path for run_config.yaml of btc
 
         # self.config.feature['large_voca'] = False
         # self.config.model['num_chords'] = 25
 
-        self.model_file = '/src/audiocraft/modules/btc/test/btc_model_large_voca.pt'
+        self.model_file = BTC_WEIGHTS_PATH
         # self.model_file = 'audiocraft/modules/btc/test/btc_model.pt'
         self.idx_to_chord = idx2voca_chord()
         self.sr = sample_rate
